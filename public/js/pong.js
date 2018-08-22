27a24007735a0ba8dfd8667a4d100fcd4f1c10b9
m1k431 <garcia.mikael@gmail.com>
2018-08-22 13:28:20 +0200

    reactJS

--- public/js/pong.js
+++ public/js/pong.js
@@ -9,5 +9,26 @@
         socket.on('pongEvt', (data) => {
             console.log(data)
         })
+        //reactJS test zone
+        const e = React.createElement
+        class submitButton extends React.Component {
+            constructor(props) {
+                super(props)
+                this.state = { clicked: false }
+            }
+            render() {
+                if (this.state.clicked) {
+                    return 'This game is under contruction, come back soon'
+                }
+                return e(
+                    'button',
+                    { onClick: () => this.setState({ clicked: true})},
+                    'Play'
+                )
+            }
+        }
+
+        const domContainer = document.querySelector('#like_button_container')
+        ReactDOM.render(e(submitButton), domContainer)
     })
 })()
