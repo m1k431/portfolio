17f84c57d5aeb9d6d86fbd268712b101fd433996
m1k431 <garcia.mikael@gmail.com>
2018-08-24 07:31:55 +0200

    clock extends

--- public/js/src/layoutNav.jsx
+++ public/js/src/layoutNav.jsx
@@ -1,10 +1,41 @@
-function tick() {
-    const element = (
-        <h1 className="myclock">{new Date().toLocaleTimeString()}</h1>
-    )
-    // highlight-next-line
-    ReactDOM.render(element, document.getElementById('myclock'))
+function FormattedDate(props) {
+    return <h1 className="myclock">{props.date.toLocaleTimeString()}</h1>
 }
   
-setInterval(tick, 1000)
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
   
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+  
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+  
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+  
+    render() {
+        return (
+            <FormattedDate date={this.state.date} />
+        )
+    }
+}
+  
+function App() {
+    return (
+        <Clock />
+    )
+}
+  
+ReactDOM.render(<App />, document.getElementById('myclock'))
