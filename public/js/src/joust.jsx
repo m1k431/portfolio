6e7e589c2deb1010a4024041acca7083cdc76bf9
m1k431 <garcia.mikael@gmail.com>
2018-08-30 22:01:06 +0200

    joust new files

---
+++ public/js/src/joust.jsx
@@ -0,0 +1,54 @@
+(() => {
+    window.addEventListener('DOMContentLoaded', () => {
+        //reactJS test zone
+        class PlayButton extends React.Component {
+            constructor(props) {
+                super(props)
+                this.state = { liked: false }
+            }
+            render() {
+                if (this.state.liked) {
+                    return (
+                        <div>
+                            <h1 id='clickable-element'>This game is under contruction.</h1>
+                            <h2>Come back soon</h2>
+                        </div>
+                    )
+                }
+                return (
+                    <button className="css3but" onClick={() => this.setState({ liked: true }) }>
+                Play
+                    </button>
+                )
+            }
+        }
+
+
+        class AccueilPong extends React.Component {
+            render() {
+                return (
+                    <div className='competen'>
+                        <h1 id="clickable-element">Realtime Multiplayer Game</h1>
+                        <form id="adm1n">
+                            <label className="pseudo">Enter your Name</label>
+                            <input className="pseudo" id="pseudo" type="text" name="pseudo"></input>
+                        </form>
+                        <PlayButton />
+                    </div>
+                )
+            }
+        }
+        ReactDOM.render(<AccueilPong />, document.querySelector('#m0ncentrageCV'))
+        //JAVASCRIPT
+        var socket = io('http://mikael.ml/')
+        var clickTitre = window.document.getElementById('clickable-element')
+        clickTitre.addEventListener('click', () => {
+            console.log('cliiiick')
+            socket.emit('pingEvt', {texte: 'Piiiing !'})
+        })
+        socket.on('pongEvt', (data) => {
+            console.log(data)
+        })
+        
+    })
+})()
