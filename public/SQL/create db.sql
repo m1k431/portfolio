b50fd92b45af7f7251530291c06971ade5bbba7a
m1k431 <garcia.mikael@gmail.com>
2020-03-03 22:53:50 +0100

    test commit vscode back osx

--- public/SQL/create db.sql
+++ public/SQL/create db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE portfolio;
-CREATE USER 'webuser'@'localhost' IDENTIFIED BY 'iop';
+CREATE USER webuser@localhost IDENTIFIED BY 'iop';
 GRANT select, insert ON portfolio.* TO webuser@localhost;
 FLUSH PRIVILEGES;
 quit
