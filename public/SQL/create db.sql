39d0228c3541bec3d457f05c88502c09bc3d485a
m1k431 <garcia.mikael@gmail.com>
2020-03-04 17:02:15 +0100

    ***reLecture Traduction en français

--- public/SQL/create db.sql
+++ public/SQL/create db.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE portfolio;
-CREATE USER webuser@localhost IDENTIFIED BY 'iop';
-GRANT select, insert ON portfolio.* TO webuser@localhost;
+--admin user
+CREATE USER 'admin' @ 'localhost' IDENTIFIED BY 'iop';
+GRANT ALL PRIVILEGES ON portfolio.* TO 'admin' @ 'localhost';
+FLUSH PRIVILEGES;
+--webuser
+CREATE USER 'webuser' @ 'localhost' IDENTIFIED BY 'iop';
+GRANT
+SELECT,
+INSERT ON portfolio.* TO 'webuser' @ 'localhost';
 FLUSH PRIVILEGES;
 quit
