2eb4b7400ecbe05814e19318fdd76e698362e654
m1k431 <garcia.mikaelgmail.com>
2020-05-24 20:26:21 +0200

    playback button + videoHeight

--- public/js/soleil.js
+++ public/js/soleil.js
@@ -1,7 +1,7 @@
 const monIntro = function () {
     //Bouton de Controle play/pause video jonglage 
-    //var m4vide0 = document.getElementById('jugglejungle')
-    //m4vide0.playbackRate = 0.75
+    /*var m4vide0 = document.getElementById('jugglejungle')
+    m4vide0.playbackRate = 0.75
     var BtnJs0nPl4Y = document.getElementById('BtnJs0nPl4Y')
     BtnJs0nPl4Y.innerHTML = 'PAUSE'
     BtnJs0nPl4Y.addEventListener('click', () => {
@@ -13,7 +13,7 @@ const monIntro = function () {
             BtnJs0nPl4Y.innerHTML = 'PLAY'
         }
     })
-    BtnJs0nPl4Y.click()
+    BtnJs0nPl4Y.click()*/
     //Blue/red menu button
     $('#intro').removeClass('css3button')
     $('#intro').addClass('css3buttonRed')
