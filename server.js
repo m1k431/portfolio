cb2092b5f6b6fd7c1aab6e04c75b700a0429b812
m1k431 <garcia.mikaelgmail.com>
2020-06-16 23:42:13 +0200

    oops

--- server.js
+++ server.js
@@ -101,6 +101,6 @@ app.use((req, res) => {
 app.use((error, req, res) => {
     res.status(500).render('404.pug')
 })
-server.listen(p0rt, '127.0.0.1', () => {
+server.listen(p0rt, '0.0.0.0', () => {
     console.log(`Listening on ${server.address().address}:${server.address().port}`)
 })
